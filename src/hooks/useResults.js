2de37274388a9588988ea090a762791dc2f711ad
martin mulder <martinmulder@me.com>
2021-05-06 16:21:20 +0200

    lesson 107

---
+++ src/hooks/useResults.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import yelp from '../api/yelp';
+
+export default () => {
+    const [results, setResults] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('');
+  
+    const searchApi = async (searchTerm) => {
+      console.log('hi there!');
+      try{
+        const response = await yelp.get('/search', {
+          params: {
+            limit: 50,
+            term: searchTerm,
+            location: 'amsterdam'
+          }
+        });
+        setResults(response.data.businesses);
+      } catch (err) {
+        setErrorMessage('something went wrong!');
+      }
+    };
+  
+  //Call searchApi when components is rendered the first time.
+  //searchApi('pasta');
+  
+  useEffect(()=> {searchApi('pasta');},[]);
+
+  return [searchApi, results, errorMessage];
+};
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,19 +1,34 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
+import ResultsList from '../components/ResultsList';
+import useResults from '../hooks/useResults' 
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
+  const [searchApi, results, errorMessage] = useResults();
+
+  const filterResultsByPrice = (price) => {
+    return results.filter(result => {
+      return result.price === price;
+    });
+  };
+
+  console.log(results);
 
   return (
     <View>
       <SearchBar
         term={term}
-        onTermChange={newTerm => setTerm(newTerm)}
-        onTermSubmit={() => console.log('term was submitted', term)}
+        onTermChange={setTerm}
+        onTermSubmit={() => {searchApi(term)}}
       />
-      <Text>Search Screen</Text>
-      <Text>{term}</Text>
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
+      <Text>we have found: {results.length}</Text>
+      <ResultsList results={filterResultsByPrice('€')} title="Cost effective"/>
+      <ResultsList results={filterResultsByPrice('€€')} title="Bit Pricier"/>
+      <ResultsList results={filterResultsByPrice('€€€')} title="Big Spender"/>
+      <ResultsList results={filterResultsByPrice('€€€€')} title="Gold Digger"/>
     </View>
   );
 };
